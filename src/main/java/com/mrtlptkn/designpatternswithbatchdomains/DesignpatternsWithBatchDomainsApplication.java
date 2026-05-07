808c4a906c35a4c5cc9b1fb7a21955f76d102305
neominalbilisim <neominal.solutions@gmail.com>
2026-05-07 11:25:47 +0300

    Initial commit

---
+++ src/main/java/com/mrtlptkn/designpatternswithbatchdomains/DesignpatternsWithBatchDomainsApplication.java
@@ -0,0 +1,13 @@
+package com.mrtlptkn.designpatternswithbatchdomains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternsWithBatchDomainsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatternsWithBatchDomainsApplication.class, args);
+    }
+
+}
